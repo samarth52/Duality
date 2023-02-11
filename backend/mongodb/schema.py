ecb2486d6c37e3c05204d93a201faf5ceea64b71
Samarth Chandna <samarthchandna52@gmail.com>
2023-02-11 04:39:23 -0500

    added schema

--- backend/mongodb/schema.py
+++ backend/mongodb/schema.py
@@ -1,47 +1,86 @@
 from pymongo import MongoClient
 import os
 
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("MONGO_DB")]
+
 vexpr = {"$jsonSchema": {
     "bsonType": "object",
-    "required": ["id"],
+    "required": ["userId"],
     "properties": {
-        "userId": "string",
-        "articles": [{
-            "original": {
-                "link": "string",
-                "sentiment": "int",
-                "topics": [{"type": "string"}],
+        "userId": {"type": "string"},
+        "articles": {
+            "type": "array",
+            "items": {
+                "bsonType": "object",
+                "properties": {
+                    "original": {
+                        "bsonType": "object",
+                        "properties": {
+                            "link": {"type": "string"},
+                            "sentiment": {"type": "number"},
+                            "topics": {
+                                "type": "array",
+                                "items": {
+                                    "type": "string",
+                                },
+                            },
+                        },
+                    },
+                    "neutral": {
+                        "bsonType": "object",
+                        "properties": {
+                            "link": {"type": "string"},
+                            "sentiment": {"type": "number"},
+                            "visited": {"type": "boolean"},
+                            # "topics": {
+                            #     "type": "array",
+                            #     "items": {
+                            #         "type": "string",
+                            #     }
+                            # },
+                        },
+                    },
+                    "opposite": {
+                        "bsonType": "object",
+                        "properties": {
+                            "link": {"type": "string"},
+                            "sentiment": {"type": "number"},
+                            "visited": {"type": "boolean"},
+                            # "topics": {
+                            #     "type": "array",
+                            #     "items": {
+                            #         "type": "string",
+                            #     },
+                            # },
+                        },
+                    },
+                },
             },
-            "neutral": {
-                "link": "string",
-                "sentiment": "int",
-                "visited": "bool",
-                # "topics": [{"type": "string"}],
+        },
+        "topics": {
+            "type": "array",
+            "items": {
+                "bsonType": "object",
+                "properties": {
+                    "topic": {"type": "string"},
+                    "count": {"type": "number"},
+                    "sentiment": {"type": "number"},
+                },
             },
-            "opposite": {
-                "link": "string",
-                "sentiment": "int",
-                "visited": "bool",
-                # "topics": [{"type": "string"}],
-            },
-        }],
-        "topics": [{
-           "topic": "string",
-           "sentiment": "int", 
-        }],
+        },
     }
 }}
 
-def get_db():
-    client = MongoClient()
-    return client[os.getenv("MONGO_DB")]
 
 def add_validator():
-    db = get_db()
     db.command("collMod", "users", validator=vexpr, validationLevel="moderate")
-    
+
+
 def recreate_collection():
-    db = get_db()
-    db.users.drop()
+    try:
+        db.users.drop()
+    except:
+        pass
     db.create_collection("users")
-    # add_validator()
+    add_validator()
