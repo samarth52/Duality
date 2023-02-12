d730fcfc0753ef42f08cf4f7faffbcf802851217
Samarth Chandna <samarthchandna52@gmail.com>
2023-02-12 12:52:00 -0500

    added props

--- frontend/components/RecapCard/RecapCard.tsx
+++ frontend/components/RecapCard/RecapCard.tsx
@@ -42,7 +42,7 @@ import Image from "next/image";
 //   );
 // }
 
-function RecapCard({articles}) {
+function RecapCard({articles} : {articles: any}) {
   const [items, setItems] = useState<any>([]);
   useEffect(() => {
     async function getItems() {
